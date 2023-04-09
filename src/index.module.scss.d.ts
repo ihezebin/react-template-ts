26ea448e4865fbe755c732a7fa33810edf2a3f3b
hezebin <86744316@qq.com>
2023-04-09 23:51:10 +0800

    allow build dir to git and some other optimize

--- src/index.module.scss.d.ts
+++ src/index.module.scss.d.ts
@@ -2,3 +2,9 @@ declare module '*.module.scss' {
     const style: { [className: string]: string };
     export default style;
 }
+
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
