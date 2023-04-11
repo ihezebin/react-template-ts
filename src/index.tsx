5dbb9af3ff890ddcb4729cf36f6a913b6bddc0d0
hezebin <86744316@qq.com>
2023-04-11 19:18:40 +0800

    add ascii banner

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,16 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+const content = `
+ _                      _     _       
+| |                    | |   (_)      
+| | _   ____ _____ ____| | _  _ ____  
+| || \\ / _  |___  ) _  ) || \\| |  _ \\ 
+| | | ( (/ / / __( (/ /| |_) ) | | | |
+|_| |_|\\____|_____)____)____/|_|_| |_|
+        `
+console.log(content)
+
 root.render(
     <BrowserRouter>
         <Router/>
