b5409162d7c4dc933a32e2c4da700c8a37de314e
hezebin <86744316@qq.com>
2024-02-18 17:47:50 +0800

    feature: add icon of component

---
+++ src/component/icon/index.tsx
@@ -0,0 +1,8 @@
+import { createFromIconfontCN } from '@ant-design/icons'
+
+/**
+ * Example: <IconFont type="icon-http" />
+ */
+export const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/c/font_4407981_b6k3ooswd0w.js',
+})
