7b3441a9898dc0b6a25118a777e0e1f60ad2375d
hezebin <86744316@qq.com>
2024-03-16 18:58:47 +0800

    feature: add eslint-plugin-import to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended', // 使用 TypeScript 推荐规则
+    // https://www.5axxw.com/wiki/content/c3wa5h
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   parser: '@typescript-eslint/parser', // 使用 TypeScript 解析器
-  plugins: ['react', '@typescript-eslint'], // 启用 TypeScript 插件
+  plugins: ['react', '@typescript-eslint', 'import'], // 启用 TypeScript 插件
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-unresolved': 'off',
+    'import/named': 'error',
+    'import/order': ['error', { 'newlines-between': 'always' }],
   },
 }
--- typing.d.ts
+++ typing.d.ts
@@ -1,15 +1,17 @@
 declare module '*.module.scss' {
-    const style: { readonly [key: string]: string }
-    export default style;
+  const style: { readonly [key: string]: string }
+  export default style
 }
 
 declare module '*.module.css' {
-    const style: { readonly [key: string]: string }
-    export default style;
+  const style: { readonly [key: string]: string }
+  export default style
 }
 
-declare module '*.png';
+declare module '*.png'
+declare module '*.jpeg'
+declare module '*.gif'
 
 declare interface IObject {
-    [key: string]: any;
+  [key: string]: any
 }
