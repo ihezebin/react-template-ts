facb862bf49f69787345159c94e9ddc9161ef483
hezebin <86744316@qq.com>
2024-03-20 22:20:57 +0800

    feature: add something to handle theme

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,14 +9,33 @@ interface User {
 }
 
 interface IStore {
+  theme: string
+  setThemeDark: () => void
+  setThemeLight: () => void
   user: User | null
   setUser: (user: User) => void
   clearUser: () => void
 }
 
 const LOCALSTORAGE_KEY_USER = 'user'
+const LOCALSTORAGE_KEY_THEME = 'theme'
 
 export const useStore = create<IStore>((set) => ({
+  theme: (() => {
+    const theme = localStorage.getItem(LOCALSTORAGE_KEY_THEME) || ''
+    document.documentElement.setAttribute(LOCALSTORAGE_KEY_THEME, theme)
+    return theme
+  })(),
+  setThemeDark: () => {
+    const theme = 'dark'
+    set((state) => ({ ...state, theme: theme }))
+    document.documentElement.setAttribute(LOCALSTORAGE_KEY_THEME, theme)
+  },
+  setThemeLight: () => {
+    const theme = 'light'
+    set((state) => ({ ...state, theme: theme }))
+    document.documentElement.setAttribute(LOCALSTORAGE_KEY_THEME, theme)
+  },
   user: JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY_USER) || 'null'),
   setUser: (user) => {
     set((state) => ({ ...state, user: { ...state.user, ...user } }))
@@ -27,7 +46,13 @@ export const useStore = create<IStore>((set) => ({
 export const unsubscribeStore = useStore.subscribe((state: IStore) => {
   if (!state?.user) {
     localStorage.removeItem(LOCALSTORAGE_KEY_USER)
-  } else {
+  } else if (state?.user) {
     localStorage.setItem(LOCALSTORAGE_KEY_USER, JSON.stringify(state.user))
+  }
+
+  if (!state?.theme) {
+    localStorage.removeItem(LOCALSTORAGE_KEY_THEME)
+  } else if (state?.theme) {
+    localStorage.setItem(LOCALSTORAGE_KEY_THEME, state.theme)
   }
 })
