b23bc80d8aaa6a2b9a4eccda5ff947fb704cac2f
hezebin <86744316@qq.com>
2024-06-14 20:48:36 +0800

    feat: optimize router routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,41 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { Navigate, useNavigate, useRoutes } from 'react-router-dom'
+import { useNavigate, useRoutes } from 'react-router-dom'
 import { KEY_TOKEN } from '@hezebin/doraemon'
 import { message, Spin } from 'antd'
 
-import GlobalLayout from '../layout'
-import Forbidden from '../page/error/forbidden'
-import Nothing from '../page/error/nothing'
 import { unsubscribeStore, useStore } from '../store'
 import { handleUnAuthorized } from '../util'
 import { api } from '../api'
-import Test from '../page/test'
 
-//  authRoute 检查认证状态，如果已认证则返回原页面组件，否则返回做无权限处理
-const authRoute = (element: React.ReactElement) => {
-  const authed = localStorage.getItem('token')
-  return authed ? element : <Navigate to={'/forbidden'} replace />
-}
+import { routes } from './routes'
 
 // https://reactrouter.com/en/6.21.1/route/route#index
 const Router = () => {
   const navigate = useNavigate()
   const { setUser, clearUser, token, setToken } = useStore()
   const [authing, setAuthing] = useState<boolean>(true)
-
-  const routes = useRoutes([
-    {
-      element: <GlobalLayout />,
-      children: [
-        { index: true, element: <Navigate to={'test/1234?a=1&b=2&c=3'} /> },
-        { path: '/test/:id', element: <Test /> },
-        { path: 'need_auth', element: authRoute(<Test />) },
-        { path: 'forbidden', element: <Forbidden /> },
-        { path: 'nothing', element: <Nothing /> },
-        { path: '*', element: <Navigate to={'nothing'} replace /> },
-      ],
-    },
-  ])
 
   useEffect(() => {
     // 如果当前地址后面带了 token query 参数，则将 token 写到本地
@@ -93,7 +71,7 @@ const Router = () => {
       spinning={authing}
       tip={'加载账号数据...'}
       size={'large'}>
-      {!authing && routes}
+      {!authing && useRoutes(routes)}
     </Spin>
   )
 }
---
+++ src/router/routes.tsx
@@ -0,0 +1,21 @@
+import { Navigate, RouteObject } from 'react-router-dom'
+import React from 'react'
+
+import GlobalLayout from '../layout'
+import Test from '../page/test'
+import Forbidden from '../page/error/forbidden'
+import Nothing from '../page/error/nothing'
+
+export const routes: RouteObject[] = [
+  {
+    element: <GlobalLayout />,
+    children: [
+      { index: true, element: <Navigate to={'test/1234?a=1&b=2&c=3'} /> },
+      { path: '/test/:id', element: <Test /> },
+      { path: 'need_auth', element: <Test /> },
+      { path: 'forbidden', element: <Forbidden /> },
+      { path: 'nothing', element: <Nothing /> },
+      { path: '*', element: <Navigate to={'nothing'} replace /> },
+    ],
+  },
+]
