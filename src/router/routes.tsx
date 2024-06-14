1cca524535c5213692735cd867c3d67c43217fa0
hezebin <86744316@qq.com>
2024-06-14 20:53:20 +0800

    feat: optimize router routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,5 @@
-import { Navigate, RouteObject } from 'react-router-dom'
+import type { RouteObject } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import React from 'react'
 
 import GlobalLayout from '../layout'
